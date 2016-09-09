132a6e86c82ae2c879cd82bdb18fde963775e0c6
Zlatko Herzl <zlatko.herzl@outlook.com>
2016-09-09 20:46:22 +0200

    Add project files.

---
+++ MVC_LoadDataOnMouseScroll/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_LoadDataOnMouseScroll.Models;
+using System.Web.Script.Serialization;
+
+namespace MVC_LoadDataOnMouseScroll.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        public ActionResult LoadCustomers(int skip, int take)
+        {
+            AdventureWorksModel context = new AdventureWorksModel();
+            List<Kupac> customers = context.Kupac.OrderBy(x => x.IDKupac).Skip(skip).Take(take).ToList();
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            //js.Serialize(customers);
+
+            var custQuery = from c in customers
+                            select new
+                            {
+                                IDKupac = c.IDKupac,
+                                Ime = c.Ime,
+                                Prezime = c.Prezime,
+                                Email = c.Email,
+                                Grad = c.Grad.Naziv
+                            };
+
+            return Json(custQuery.ToList());
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC_LoadDataOnMouseScroll/Models/Kupac.cs
@@ -0,0 +1,34 @@
+namespace MVC_LoadDataOnMouseScroll.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Kupac")]
+    public partial class Kupac
+    {
+        [Key]
+        public int IDKupac { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Ime { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Prezime { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(25)]
+        public string Telefon { get; set; }
+
+        public int? GradID { get; set; }
+
+        public virtual Grad Grad { get; set; }
+
+    }
+}
