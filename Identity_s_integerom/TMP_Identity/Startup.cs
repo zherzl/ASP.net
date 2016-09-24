fce6ffaa38aa0cbe0324bfd5ea6a669ce66b33cf
zherzl <zlatko.herzl@outlook.com>
2016-09-24 11:18:21 +0200

    Inicijalni commit za identity

---
+++ Identity_s_integerom/TMP_Identity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TMP_Identity.Startup))]
+namespace TMP_Identity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
