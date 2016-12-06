112f675b8e158d097ce0f8778fe747a71c506e4c
zherzl <zlatko.herzl@outlook.com>
2016-12-06 12:09:39 +0100

    Just commit

---
+++ Knockout_Learn1/Knockout_Learn1/Scripts/ko/formatters.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    //console.log(formatValue("10.99"));
+});
+
+
+function formatValue(price) {
+    var n, x, s, c;
+    n = 2;
+    x = price;
+    s = '.';
+    c = ',';
+
+    var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+    num = x.replace('.', ','); //x.toFixed(Math.max(0, ~~n));
+        //num = x;
+    return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+}
--- Knockout_Learn1/Knockout_Learn1/Scripts/ko/ko.js
+++ Knockout_Learn1/Knockout_Learn1/Scripts/ko/ko.js
@@ -1,7 +1,13 @@
 ﻿function Product(name, price) {
     this.name = ko.observable(name);
     this.price = ko.observable(price);
+    this.priceFormatted = ko.computed(function () {
+
+        return formatValue(this.price().toString());
+    }, this);
+        
 }
+
 
 function personModel() {
     // self je referencea koja se koristi za brisanje
@@ -12,18 +18,20 @@ function personModel() {
         alert('Stisnuo si ' + this.lastName());
     };
 
+    this.visina = ko.observable(formatValue("180.53"));
+
     this.spremiPodatke = function () {
         spremiPodatke(this);
     };
 
     this.shoppingCart = ko.observableArray(
         [
-            new Product("Pivo", 10.99),
+            new Product("Pivo", 100000.999),
             new Product("Pecivo", 12.99),
             new Product("Patka", 8.99)
         ]);
 
-    // remove metoda je korisna za real-time objekte...kod slanja na na server može biti problem. 
+    // remove metoda je korisna za real-time objekte...kod slanja na server može biti problem. 
     // Objašnjenje:
     //For example, consider the task of saving the shopping cart to a database every time the user added or deleted an item. 
     //With remove(), the item is removed immediately, so all you can do is send your server the new list in its entirety—it’s impossible to determine which items where added or removed. 
@@ -31,9 +39,10 @@ function personModel() {
     this.removeProduct = function (proizvod) {
         //self.shoppingCart.remove(proizvod);
         // Dobar način u tom slučaju koji ne uklanja item iz liste zapravo:
-        //self.shoppingCart.destroy(proizvod);
-        alert(self.shoppingCart().length + " " + proizvod.name());
-        
+        self.shoppingCart.destroy(proizvod);
+        //alert(proizvod._destroy);
+        //alert(self.shoppingCart().length + " " + proizvod.name());
+        ispisiSve(self.shoppingCart());
     };
 
 
@@ -45,6 +54,30 @@ function personModel() {
         return this.firstName() + ' ' + this.lastName();
     }, this);
 };
+
+
+function ispisiSve(cart) {
+
+    for (var i = 0; i < cart.length; i++) {
+        console.log(cart[i]._destroy);
+    }
+}
+
+
+Product.prototype.formatMoney = function (c, d, t) {
+    var n = this,
+        c = isNaN(c = Math.abs(c)) ? 2 : c,
+        d = d == undefined ? "." : d,
+        t = t == undefined ? "," : t,
+        s = n < 0 ? "-" : "",
+        i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))),
+        j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
+
+
+
+
 
 function spremiPodatke(data) {
     alert(data.fullName());
